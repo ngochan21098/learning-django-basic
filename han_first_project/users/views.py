ca212ee47091075bb76a7e9a0e2add08ec8b3fac
ngochan21098 <ngochan21098@gmail.com>
2024-10-14 21:30:10 +0700

    1410 newest code

--- han_first_project/users/views.py
+++ han_first_project/users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Ticket 
 
 # Create your views here.
 
 def create_user(req):
+    return HttpResponse('OK')
+
+def ticket_create(req):
     return HttpResponse('OK')
